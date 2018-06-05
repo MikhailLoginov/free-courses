dffd547f887e30305592a811f6b696dec798f478
Mikhail <madion86@yandex.ru>
2018-06-05 13:44:19 +0300

    2nd task days validation

--- task2.js
+++ task2.js
@@ -5,7 +5,8 @@
 var d = parseInt(process.argv[2]),
     m = process.argv[3],
     y = parseInt(process.argv[4]),
-    counter = 0;
+    counter = 0,
+    err = false;
 
 // if (d >= 1 && d <= 31 && y >= 1 && y <= 2999) {
     switch (m) {
@@ -20,18 +21,27 @@ var d = parseInt(process.argv[2]),
             if (y % 4 == 0) {
                 counter++;
             };
+            if (d > 29 || (d = 29 && y % 4 != 0)) {
+                err = true;
+            }
             break;
         case "марта":
             counter = 365 - 31 - 28 - d;
             break;
         case "апреля":
             counter = 365 - 31 - 28 - 31 - d;
+            if (d=31) {
+                err = true;
+            }
             break;
         case "мая":
             counter = 365 - 31 - 28 - 31 - 30 - d;
             break;
         case "июня":
             counter = 365 - 31 - 28 - 31 - 30 - 31 - d;
+            if (d=31) {
+                err = true;
+            }
             break;
         case "июля":
             counter = 365 - 31 - 28 - 31 - 30 - 31 - 30 - d;
@@ -41,16 +51,25 @@ var d = parseInt(process.argv[2]),
             break;
         case "сентября":
             counter = 30 + 31 + 30 + 31 - d;
+            if (d=31) {
+                err = true;
+            }
             break;
         case "октября":
             counter = 31 + 30 + 31 - d;
             break;
         case "ноября":
             counter = 30 + 31 - d;
+            if (d=31) {
+                err = true;
+            }
             break;
         case "декабря":
             counter = 31 - d;
             break;
     }
-    process.stdout.write(`${counter}`);
+    if (!err) {
+        process.stdout.write(`${counter}`);
+    }
+    
 // }
